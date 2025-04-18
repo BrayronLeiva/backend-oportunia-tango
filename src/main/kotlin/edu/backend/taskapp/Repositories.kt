b04e50445f2648a4c74badda7a744ab8ef22beb7
BrayronLeiva <brayronleivas.com>
2025-04-18 10:13:32 -0600

    Se crea el Student.kt y el StudentRepository

--- src/main/kotlin/edu/backend/taskapp/Repositories.kt
+++ src/main/kotlin/edu/backend/taskapp/Repositories.kt
@@ -1,5 +1,6 @@
 package edu.backend.taskapp
 
+import edu.backend.taskapp.Entities.Student
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
@@ -8,3 +9,6 @@ interface PriorityRepository: JpaRepository<Priority, Long>
 
 @Repository
 interface TaskRepository: JpaRepository<Task, Long>
+
+@Repository
+interface StudentRepository: JpaRepository<Student, Long>
